4446d6e7cad95e756809c4d19ad9ed379908b69a
Chencsi <kevinkeeeh@gmail.com>
2025-08-17 20:08:24 +0200

    404 view created

---
+++ src/views/NotFound.jsx
@@ -0,0 +1,9 @@
+function NotFound() {
+    return (
+        <div className="h-screen w-screen flex flex-col items-center justify-center">
+            <h1>Hoppá!</h1>
+        </div>
+    )
+}
+
+export default NotFound;
