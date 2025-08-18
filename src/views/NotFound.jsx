18f73a2041b845c3e2b8da3465206730794e235a
Chencsi <kevinkeeeh@gmail.com>
2025-08-18 11:52:55 +0200

    404 page added

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,7 +1,13 @@
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 function NotFound() {
+    const naviate = useNavigate();
     return (
-        <div className="h-screen w-screen flex flex-col items-center justify-center">
-            <h1>Hoppá!</h1>
+        <div className="h-screen w-screen flex flex-col items-center justify-center gap-3">
+            <h1 className="text-4xl font-extrabold">Hoppá!</h1>
+            <p>Sajnos az általad keresett oldal nem létezik. :(</p>
+            <Button variant="contained" onClick={() => naviate("/")}>Vissza a főoldalra</Button>
         </div>
     )
 }
