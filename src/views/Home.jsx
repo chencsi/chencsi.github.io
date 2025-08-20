2770935690e09d4d5e78c28f6253df48f90d8d38
Chencsi <kevinkeeeh@gmail.com>
2025-08-20 15:20:25 +0200

    portrait added to home and apply

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,11 @@
 function Home() {
-    return (
-        <>
-        <h1>Welcome Home!</h1>
-        </>
-    )
+	return (
+		<div>
+			<div className="w-60 h-60 overflow-hidden rounded-full bg-blue-400 select-none pointer-events-none">
+				<img src="/portrait.png" alt="'Portrait photo'" className="w-full h-full object-cover object-[50%_15%]"/>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
