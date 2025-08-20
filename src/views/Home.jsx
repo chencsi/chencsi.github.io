93523d97cb539ffec475540de0e41f3e414aaba9
Chencsi <kevinkeeeh@gmail.com>
2025-08-21 01:54:16 +0200

    add gap to home screen

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import Highlights from "../components/Highlights";
 
 function Home() {
 	return (
-		<main>
+		<main className="flex flex-col gap-10">
 			<Hero />
+            <Highlights />
 		</main>
 	);
 }
