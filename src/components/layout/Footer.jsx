d10c38b3fc5ae72329bb965cfec547231d43cfcc
Chencsi <kevinkeeeh@gmail.com>
2025-08-21 12:17:59 +0200

    base footer

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,26 @@
+import navItems from "./navItems";
+
+function Footer() {
+	return (
+		<div className="h-50 bg-zinc-400/10 py-5 flex flex-col justify-between">
+			<ul className="flex flex-row gap-5 justify-center">
+				{navItems
+					.filter((item) => item.footer === true)
+					.map((item) => (
+						<li key={item.path}>
+							<a
+								href={item.path}
+								className="text-gray-400 hover:text-white transition-[color] duration-300"
+							>
+								{item.title}
+							</a>
+						</li>
+					))}
+			</ul>
+            
+			<p className="text-center text-gray-400">Kód és design: Kevin Chen 💙 · © 2025</p>
+		</div>
+	);
+}
+
+export default Footer;
