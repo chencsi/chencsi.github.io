9a8140212b97322f54a6abf1aaaece8e3058d355
Chencsi <kevinkeeeh@gmail.com>
2025-08-21 21:50:26 +0200

    sync mui breakpoints with tailwind breakpoints

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,11 +1,20 @@
-import { createTheme, ThemeProvider} from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#60a5fa' // blue-400
-        }
-    }
+	palette: {
+		primary: {
+			main: "#60a5fa", // blue-400
+		},
+	},
+	breakpoints: {
+		values: {
+			xs: 0,
+			sm: 640,
+			md: 768,
+			lg: 1024,
+			xl: 1280,
+		},
+	},
 });
 
 export default theme;
