f1486849392f62d6c2e47032680c9a6e0cbe1999
Chencsi <kevinkeeeh@gmail.com>
2025-08-22 11:34:42 +0200

    rename theme file

---
+++ src/themes/muiTheme.js
@@ -0,0 +1,20 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const muiTheme = createTheme({
+	palette: {
+		primary: {
+			main: "#60a5fa", // blue-400
+		},
+	},
+	breakpoints: {
+		values: {
+			xs: 0,
+			sm: 640,
+			md: 768,
+			lg: 1024,
+			xl: 1280,
+		},
+	},
+});
+
+export default muiTheme;
