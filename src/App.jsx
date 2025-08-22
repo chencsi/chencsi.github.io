ccd6aa20e9016ae1a93939bb9dd415a5a6db7074
Chencsi <kevinkeeeh@gmail.com>
2025-08-22 11:35:03 +0200

    add ui context provider to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import { RouterProvider } from "react-router-dom";
 import { routers } from "./routes";
 import { ThemeProvider } from "@mui/material";
-import theme from "./theme/theme";
+import UIContext from "./themes/UIContext";
+import muiTheme from "./themes/muiTheme";
+import { useState } from "react";
 
 function App() {
+	const [theme, setTheme] = useState('light');
+	const [lang, setLang] = useState('hu');
+
+	const toggleTheme = () => setTheme(prev => prev === 'light' ? 'dark' : 'light');
+	const setLanguage = (newLang) => setLang(newLang);
+
+	const contextValue = {
+		theme,
+		lang,
+		toggleTheme,
+		setLanguage
+	}
+
 	return (
-		<ThemeProvider theme={theme}>
-			<RouterProvider router={routers} />
+		<ThemeProvider theme={muiTheme}>
+			<UIContext.Provider value={contextValue}>
+				<RouterProvider router={routers} />
+			</UIContext.Provider>
 		</ThemeProvider>
 	);
 }
