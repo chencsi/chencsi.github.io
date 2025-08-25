1c17293a881b80b678c6eaadc3dc0f91c7a064f9
Chencsi <kevinkeeeh@gmail.com>
2025-08-25 13:49:22 +0200

    aos added

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,22 @@ import { routers } from "./routes";
 import { ThemeProvider } from "@mui/material";
 import muiTheme from "./themes/muiTheme";
 import { UIProvider } from "./contexts/UIContext";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 function App() {
+	useEffect(() => {
+		AOS.init({
+			duration: 800,
+			easing: "ease-in",
+			once: true,
+			mirror: true,
+			offset: 100,
+			delay: 0,
+		})
+	}, []);
+
 	return (
 		<ThemeProvider theme={muiTheme}>
 			<UIProvider>
--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -11,7 +11,7 @@ const Layout = () => {
 			<nav className="w-full overflow-hidden h-25">
 				<Navigation />
 			</nav>
-			<main className="pt-5 px-3 flex flex-col gap-20 min-h-screen">
+			<main className="pb-10 px-3 flex flex-col gap-20 min-h-screen">
 				<Outlet />
 			</main>
 			<footer>
