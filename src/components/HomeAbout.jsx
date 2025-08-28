5660c1c34e83335ab7fadc273ec6f540e8550fdc
Chencsi <kevinkeeeh@gmail.com>
2025-08-28 11:32:33 +0200

    HomeAbout created

---
+++ src/components/HomeAbout.jsx
@@ -0,0 +1,11 @@
+function HomeAbout() {
+	return (
+		<div className="flex flex-col gap-5">
+			<h1 className="text-zinc-300 font-extrabold text-5xl uppercase text-shadow">Rólam</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus libero, dolores veritatis fugiat eaque aperiam minus quae! Exercitationem odit atque hic molestiae porro, iusto earum harum, est nostrum vero sequi.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non quia repellendus incidunt velit modi aut consequuntur placeat, nihil nisi sunt, ipsam hic saepe optio eveniet ipsa quis dicta aliquam perferendis?</p>
+        </div>
+	);
+}
+
+export default HomeAbout;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,14 @@
+import HomeAbout from "../components/HomeAbout";
 import HomeHero from "../components/HomeHero";
 import HomeHighlights from "../components/HomeHighlights";
 
 function Home() {
 	return (
-		<>
+		<div className="h-full flex flex-col gap-y-34">
 			<HomeHero />
 			<HomeHighlights />
-		</>
+			<HomeAbout />
+		</div>
 	);
 }
 
