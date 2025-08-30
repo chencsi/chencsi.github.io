943daa4bad50f3f41ac2d788921606b28694fb52
Chencsi <kevinkeeeh@gmail.com>
2025-08-31 01:20:42 +0200

    about now uses translation and theme

--- src/components/HomeAbout.jsx
+++ src/components/HomeAbout.jsx
@@ -1,18 +1,24 @@
+import translations from "../utils/translations";
+import useUI from "../hooks/useUI";
+
 function HomeAbout() {
+	const { theme, lang } = useUI();
+	const content = translations[lang]?.pages?.home?.about;
+
 	return (
 		<div
 			data-aos="fade-up"
 			data-aos-duration="500"
 			className="flex flex-col gap-2 max-w-[1000px] mx-auto"
 		>
-			<h2 className="text-zinc-300 font-extrabold text-5xl uppercase text-shadow mb-5">
-				Rólam
-			</h2>
+			<h3 className={`${theme === "dark" ? "text-zinc-300" : "text-zinc-700"} font-extrabold text-5xl uppercase text-shadow mb-5`}>
+				{content?.h3}
+			</h3>
 			<p>
-				A nevem Kevin és {0} éve kezdtem el foglalkozni programozással, azonban a programozás iránti érdeklődésem egészen általános iskolás koromra nyúlik vissza. Azóta abszolút elszánt vagyok iránta. Ez idő alatt egy szoftverfejlesztő és -tesztelő technikusi kézpést végeztem el, most pedig az egyetemi éveim kezdem meg mérnökinformatikus szakon. Eddig számos iskolai és saját projektben vettem részt, ezekre pontosabban a <a href="/references" className="text-blue-400 hover:underline">referenciáknál</a> térek ki.
+				{content?.p1}
 			</p>
 			<p>
-				Célom, hogy felhasználóbarát weboldalakat és webalkalmazásokat építsek, amik valós problémákra találnak megoldást. Folyamatosan naprakészen tartom a tudástáram, hogy a legújabb technológiákat használjam.
+				{content?.p2}
 			</p>
 		</div>
 	);
