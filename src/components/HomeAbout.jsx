3d6e677ad36c0927c4acae5da1f51e1ee7e7d46c
Chencsi <kevinkeeeh@gmail.com>
2025-08-31 02:07:51 +0200

    about re-styling

--- src/components/HomeAbout.jsx
+++ src/components/HomeAbout.jsx
@@ -6,21 +6,29 @@ function HomeAbout() {
 	const content = translations[lang]?.pages?.home?.about;
 
 	return (
-		<div
-			data-aos="fade-up"
-			data-aos-duration="500"
-			className="flex flex-col gap-2 max-w-[1000px] mx-auto"
-		>
-			<h3 className={`${theme === "dark" ? "text-zinc-300" : "text-zinc-700"} font-extrabold text-5xl uppercase text-shadow mb-5`}>
-				{content?.h3}
-			</h3>
-			<p className={`${theme === "dark" ? "text-zinc-300" : "text-zinc-900"}`}>
-				{content?.p1}
-			</p>
-			<p className={`${theme === "dark" ? "text-zinc-300" : "text-zinc-900"}`}>
-				{content?.p2}
-			</p>
-		</div>
+		<section className={`${theme === "dark" ? "bg-zinc-900" : "bg-zinc-100"}`}>
+			<div
+				data-aos="fade-right"
+				data-aos-duration="500"
+				className="p-10 flex flex-col gap-2 max-w-[1000px] mx-auto"
+			>
+				<h3
+					className={`${theme === "dark" ? "text-zinc-300" : "text-zinc-700"} font-extrabold text-5xl uppercase text-shadow mb-5`}
+				>
+					{content?.h3}
+				</h3>
+				<p
+					className={`${theme === "dark" ? "text-zinc-300" : "text-zinc-900"}`}
+				>
+					{content?.p1}
+				</p>
+				<p
+					className={`${theme === "dark" ? "text-zinc-300" : "text-zinc-900"}`}
+				>
+					{content?.p2}
+				</p>
+			</div>
+		</section>
 	);
 }
 
