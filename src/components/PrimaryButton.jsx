8dd2c671da5b5f24509238824a55d8a94b31bc7b
Chencsi <kevinkeeeh@gmail.com>
2025-09-17 10:54:16 +0200

    create own button component

---
+++ src/components/PrimaryButton.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+const PrimaryButtonBase = ({ children, theme }) => {
+  return (
+    <div>
+      <button
+        type="button"
+        className="mt-2 bg-gradient-to-br from-blue-500 to-indigo-500 px-5 py-1 rounded-lg text-lg text-zinc-50 hover:from-blue-600 hover:to-indigo-600 hover:scale-105 cursor-pointer transition"
+      >
+        {children}
+      </button>
+    </div>
+  );
+};
+
+const PrimaryButton = ({ children, link, theme }) => {
+  if (link !== undefined) {
+    return (
+      <Link to={link}>
+        <PrimaryButtonBase>{children}</PrimaryButtonBase>
+      </Link>
+    );
+  }
+  return <PrimaryButtonBase>{children}</PrimaryButtonBase>;
+};
+
+export default PrimaryButton;
