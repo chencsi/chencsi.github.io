5b9cb4239ec2ac4dbaf540ef271ad11ef88e7c0d
Chencsi <kevinkeeeh@gmail.com>
2025-09-17 10:54:30 +0200

    use button

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -2,6 +2,7 @@ import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import useUI from "../hooks/useUI";
 import translations from "../utils/translations";
+import PrimrayButton from "./PrimaryButton";
 
 function Hero() {
 	const { theme, lang } = useUI();
@@ -9,7 +10,7 @@ function Hero() {
 
 	return (
 		<section
-			className={`${theme === "dark" ? "bg-gradient-to-br from-zinc-800 to-zinc-900 text-zinc-100 border-zinc-800" : "bg-zinc-100 text-zinc-700"} border rounded-4xl shadow-xl flex sm:flex-row items-center flex-col-reverse justify-between gap-5 sm:gap-15 py-10 px-5 sm:py-15 sm:px-10 md:px-20 max-w-[600px] md:max-w-[800px] mx-auto`}
+			className={`${theme === "dark" ? "bg-gradient-to-br from-zinc-800 to-zinc-900 text-zinc-100 border-zinc-800" : "bg-zinc-100 text-zinc-700"} border rounded-4xl shadow-xl flex sm:flex-row items-center flex-col-reverse justify-between gap-5 sm:gap-10 lg:gap-15 py-10 px-5 sm:py-15 sm:px-10 md:px-20 max-w-[600px] md:max-w-[800px] mx-auto`}
 		>
 			<div className="flex flex-col self-center text-center sm:text-left sm:gap-1">
 				<h1 className="sm:text-left text-4xl sm:text-4xl md:text-5xl font-bold">
@@ -25,14 +26,9 @@ function Hero() {
 				>
 					{content?.p}
 				</p>
-				<Link to={"/contact"}>
-					<button
-						type="button"
-						className="mt-2 bg-gradient-to-br from-blue-500 to-indigo-500 px-3 py-1 rounded-lg text-lg text-zinc-50 hover:from-blue-600 hover:to-indigo-600 hover:scale-105 cursor-pointer transition"
-					>
-						{content?.button}
-					</button>
-				</Link>
+				<PrimrayButton link="/contact">
+          {content?.button}
+        </PrimrayButton>
 			</div>
 			<div className="block max-w-50 max-h-50 md:max-w-60 md:max-h-60 overflow-hidden rounded-full bg-gradient-to-br from-blue-500 to-indigo-500 select-none pointer-events-none shadow-xl">
 				<img
--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,16 +1,13 @@
-import { Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import PrimaryButton from "../components/PrimaryButton";
 
 function NotFound() {
-	const naviate = useNavigate();
-
 	return (
 			<div className="h-[70vh] flex flex-col items-center justify-center gap-3">
 				<h1 className="text-4xl font-extrabold">Hoppá!</h1>
 				<p>Sajnos az általad keresett oldal nem létezik. :(</p>
-				<Button variant="contained" onClick={() => naviate("/")}>
-					Vissza a főoldalra
-				</Button>
+				<PrimaryButton link="/">
+          Vissza a főoldalra
+        </PrimaryButton>
 			</div>
 	);
 }
