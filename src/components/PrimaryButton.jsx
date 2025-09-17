1df1684e166eee96490c974ad7b541adf1a5f704
Chencsi <kevinkeeeh@gmail.com>
2025-09-17 13:19:23 +0200

    small bugfix

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -2,23 +2,23 @@ import { Link } from "react-router-dom";
 
 const PrimaryButtonBase = ({ children, theme }) => {
   return (
-    <div>
-      <button
-        type="button"
-        className="mt-2 bg-gradient-to-br from-blue-500 to-indigo-500 px-5 py-1 rounded-lg text-lg text-zinc-50 hover:from-blue-600 hover:to-indigo-600 hover:scale-105 cursor-pointer transition"
-      >
-        {children}
-      </button>
-    </div>
+    <button
+      type="button"
+      className="mt-2 bg-gradient-to-br from-blue-500 to-indigo-500 px-5 py-1 rounded-lg text-lg text-zinc-50 hover:from-blue-600 hover:to-indigo-600 hover:scale-105 cursor-pointer transition"
+    >
+      {children}
+    </button>
   );
 };
 
 const PrimaryButton = ({ children, link, theme }) => {
   if (link !== undefined) {
     return (
-      <Link to={link}>
-        <PrimaryButtonBase>{children}</PrimaryButtonBase>
-      </Link>
+      <div>
+        <Link to={link}>
+          <PrimaryButtonBase>{children}</PrimaryButtonBase>
+        </Link>
+      </div>
     );
   }
   return <PrimaryButtonBase>{children}</PrimaryButtonBase>;
