4f9546e6e791f157b856f0bb14b3e247f6456c89
Chencsi <kevinkeeeh@gmail.com>
2025-09-17 10:53:51 +0200

    translations added and use

--- src/utils/translations.js
+++ src/utils/translations.js
@@ -1,132 +1,235 @@
 const sinceYear = new Date().getFullYear() - 2020;
 
 const translations = {
-	hu: {
-		layout: {
-			nav: {
-				links: {
-					home: "Főoldal",
-					references: "Referenciák",
-					contact: "Kapcsolat",
-				},
-				settings: {
-					darkmode: "Sötét mód",
-					language: "Nyelv",
-				},
-			},
-            footer: {
-                p: "Kód és design: Kevin Chen 💙 · © 2025"
-            }
-		},
-		pages: {
-			home: {
-				hero: {
-					h1: "Kevin Chen",
-					h2: "Frontend fejlesztő hallgató",
-					p: "Letisztult és felhasználóbarát weboldalakat építek",
-					button: "Kapcsolat",
-					imgAlt: "Portré fotó",
-				},
-				highlights: {
-					blue: {
-						h3: "Reszponzív weboldalak",
-						p: "Minden eszközre optimalizált megjelenés.",
-					},
-					yellow: {
-						h3: "Modern technológiák",
-						p: "React, Angular, Laravel és TailwindCSS",
-					},
-					green: {
-						h3: "UI/UX fókusz",
-						p: "Letisztult és felhasználóbarát felhasználói élmény.",
-					},
-				},
-				about: {
-					h3: "Rólam",
-					p1: `A nevem Kevin és ${sinceYear} éve kezdtem el foglalkozni programozással, azonban a programozás iránti érdeklődésem egészen általános iskolás koromra nyúlik vissza. Azóta abszolút elszánt vagyok iránta. Ez idő alatt egy szoftverfejlesztő és -tesztelő technikusi képzést végeztem el, most pedig az egyetemi éveim kezdem meg mérnökinformatikus szakon. Eddig számos iskolai és saját projektben vettem részt, ezekre pontosabban a <a href="/references" class="text-blue-400 hover:underline">referenciáknál</a> térek ki.`,
-					p2: "Célom, hogy felhasználóbarát weboldalakat és webalkalmazásokat építsek, amik valós problémákra találnak megoldást. Folyamatosan naprakészen tartom a tudástáram, hogy a legújabb technológiákat használjam.",
-				},
-				education: {
-					h3: "Tanulmányok",
-					schools: [
-						{
-							h5: "Gábor Dénes Egyetem",
-							p1: "2025-",
-							p2: "Jelenleg az első félévemet töltöm itt, mérnökinformatikus szakon és izgatottan várom, milyen további skilleket sajátíthatok még itt el.",
-						},
-						{
-							h5: "BMSZC Neumann János Informatikai Technikum",
-							p1: "2020-2025",
-							p2: "Itt betekintést nyerhettem a Full-Stack fejlesztésbe, adatbázis kezelést és konténerizációt tanultunk.",
-						},
-					],
-				},
-			},
-		},
-	},
-	en: {
-		layout: {
-			nav: {
-				links: {
-					home: "Home",
-					references: "References",
-					contact: "Contact",
-				},
-				settings: {
-					darkmode: "Dark mode",
-					language: "Language",
-				},
-			},
-            footer: {
-                p: "Code & design: Kevin Chen 💙 · © 2025"
-            }
-		},
-		pages: {
-			home: {
-				hero: {
-					h1: "Kevin Chen",
-					h2: "Frontend Developer Student",
-					p: "I build clean and user-friendly websites",
-					button: "Contact",
-					imgAlt: "Portrait photo",
-				},
-				highlights: {
-					blue: {
-						h3: "Responsive websites",
-						p: "Optimized appearance for all devices.",
-					},
-					yellow: {
-						h3: "Modern technologies",
-						p: "React, Angular, Laravel and TailwindCSS",
-					},
-					green: {
-						h3: "UI/UX focus",
-						p: "Clean and user-friendly user experience.",
-					},
-				},
-				about: {
-					h3: "About me",
-					p1: `My name is Kevin and I started programming ${sinceYear} years ago, but my interest in programming goes back to elementary school. Since then, I have been absolutely dedicated to it. During this time, I completed a software developer and tester technician course, and now I am starting my university years as an engineering IT student. So far, I have participated in several school and personal projects, which I detail more precisely in the <a href="/references" class="text-blue-400 hover:underline">references</a> section.`,
-					p2: "My goal is to build user-friendly websites and web applications that solve real problems. I constantly keep my knowledge up to date to use the latest technologies.",
-				},
-				education: {
-					h3: "Education",
-					schools: [
-						{
-							h5: "Gábor Dénes University",
-							p1: "2025-",
-							p2: "Currently in my first semester here as an engineering IT student, and I am excited to see what further skills I can acquire.",
-						},
-						{
-							h5: "BMSZC Neumann János Informatics Technical School",
-							p1: "2020-2025",
-							p2: "Here I gained insight into Full-Stack development, learned about database management and containerization.",
-						},
-					],
-				},
-			},
-		},
-	},
+  hu: {
+    layout: {
+      nav: {
+        links: {
+          home: "Főoldal",
+          references: "Referenciák",
+          contact: "Kapcsolat",
+        },
+        settings: {
+          darkmode: "Sötét mód",
+          language: "Nyelv",
+        },
+      },
+      footer: {
+        p: "Kód és design: Kevin Chen 💙 · © 2025"
+      }
+    },
+    pages: {
+      home: {
+        hero: {
+          h1: "Kevin Chen",
+          h2: "Frontend fejlesztő hallgató",
+          p: "Letisztult és felhasználóbarát weboldalakat építek",
+          button: "Kapcsolat",
+          imgAlt: "Portré fotó",
+        },
+        highlights: [
+          {
+            h3: "Reszponzív weboldalak",
+            p: "Minden eszközre optimalizált megjelenés.",
+          },
+          {
+            h3: "Modern technológiák",
+            p: "React, Angular, Laravel és TailwindCSS",
+          },
+          {
+            h3: "UI/UX fókusz",
+            p: "Letisztult és felhasználóbarát felhasználói élmény.",
+          },
+        ],
+        about: {
+          h3: "Rólam",
+          p1: `A nevem Kevin és ${sinceYear} éve kezdtem el foglalkozni programozással, azonban a programozás iránti érdeklődésem egészen általános iskolás koromra nyúlik vissza. Azóta abszolút elszánt vagyok iránta. Ez idő alatt egy szoftverfejlesztő és -tesztelő technikusi képzést végeztem el, most pedig az egyetemi éveim kezdem meg mérnökinformatikus szakon. Eddig számos iskolai és saját projektben vettem részt, ezekre pontosabban a <a href="/references" class="text-blue-400 hover:underline">referenciáknál</a> térek ki.`,
+          p2: "Célom, hogy felhasználóbarát weboldalakat és webalkalmazásokat építsek, amik valós problémákra találnak megoldást. Folyamatosan naprakészen tartom a tudástáram, hogy a legújabb technológiákat használjam.",
+        },
+        education: {
+          h3: "Tanulmányok",
+          schools: [
+            {
+              h5: "Gábor Dénes Egyetem",
+              p1: "2025-",
+              p2: "Jelenleg az első félévemet töltöm itt, mérnökinformatikus szakon és izgatottan várom, milyen további skilleket sajátíthatok még itt el.",
+            },
+            {
+              h5: "BMSZC Neumann János Informatikai Technikum",
+              p1: "2020-2025",
+              p2: "Itt betekintést nyerhettem a Full-Stack fejlesztésbe, adatbázis kezelést és konténerizációt tanultunk.",
+            },
+          ],
+        },
+      },
+      references: {
+        h3: "Referenciák",
+        references: [
+          {
+            h4: "Uranus",
+            img: "/assets/uranus.jpg",
+            badges: [
+              "React Native",
+              "Expo",
+              "Nativewind"
+            ],
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum minima excepturi, velit odit dolorum sed expedita ducimus commodi reprehenderit, iure consectetur, voluptatem ex consequatur quia dolorem asperiores perspiciatis temporibus voluptas?"
+          },
+          {
+            h4: "Vagy idén vagy soha",
+            img: "/assets/vagyidenvagysoha.jpg",
+            badges: [
+              "React",
+              "Vite",
+              "MaterialUI",
+              "TailwindCSS"
+            ],
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum minima excepturi, velit odit dolorum sed expedita ducimus commodi reprehenderit, iure consectetur, voluptatem ex consequatur quia dolorem asperiores perspiciatis temporibus voluptas?"
+          },
+          {
+            h4: "Web Task Evaulator (WTE)",
+            img: "/assets/wte.jpg",
+            badges: [
+              "Vue",
+              "Pinia",
+              "FormKit",
+              "Laravel",
+              "TailwindCSS"
+            ],
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum minima excepturi, velit odit dolorum sed expedita ducimus commodi reprehenderit, iure consectetur, voluptatem ex consequatur quia dolorem asperiores perspiciatis temporibus voluptas?"
+          },
+          {
+            h4: "WTE - Chrome Extension",
+            img: "/assets/extension.jpg",
+            badges: [
+              "Vue",
+              "Pinia",
+              "FormKit",
+              "Laravel",
+              "TailwindCSS"
+            ],
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum minima excepturi, velit odit dolorum sed expedita ducimus commodi reprehenderit, iure consectetur, voluptatem ex consequatur quia dolorem asperiores perspiciatis temporibus voluptas?"
+          },
+        ],
+      },
+      contact: {
+        h3: "Kapcsolat",
+      }
+    },
+  },
+  en: {
+    layout: {
+      nav: {
+        links: {
+          home: "Home",
+          references: "References",
+          contact: "Contact",
+        },
+        settings: {
+          darkmode: "Dark mode",
+          language: "Language",
+        },
+      },
+      footer: {
+        p: "Code & design: Kevin Chen 💙 · © 2025"
+      }
+    },
+    pages: {
+      home: {
+        hero: {
+          h1: "Kevin Chen",
+          h2: "Frontend Developer Student",
+          p: "I build clean and user-friendly websites",
+          button: "Contact",
+          imgAlt: "Portrait photo",
+        },
+        highlights: {
+          blue: {
+            h3: "Responsive websites",
+            p: "Optimized appearance for all devices.",
+          },
+          yellow: {
+            h3: "Modern technologies",
+            p: "React, Angular, Laravel and TailwindCSS",
+          },
+          green: {
+            h3: "UI/UX focus",
+            p: "Clean and user-friendly user experience.",
+          },
+        },
+        about: {
+          h3: "About me",
+          p1: `My name is Kevin and I started programming ${sinceYear} years ago, but my interest in programming goes back to elementary school. Since then, I have been absolutely dedicated to it. During this time, I completed a software developer and tester technician course, and now I am starting my university years as an engineering IT student. So far, I have participated in several school and personal projects, which I detail more precisely in the <a href="/references" class="text-blue-400 hover:underline">references</a> section.`,
+          p2: "My goal is to build user-friendly websites and web applications that solve real problems. I constantly keep my knowledge up to date to use the latest technologies.",
+        },
+        education: {
+          h3: "Education",
+          schools: [
+            {
+              h5: "Gábor Dénes University",
+              p1: "2025-",
+              p2: "Currently in my first semester here as an engineering IT student, and I am excited to see what further skills I can acquire.",
+            },
+            {
+              h5: "BMSZC Neumann János Informatics Technical School",
+              p1: "2020-2025",
+              p2: "Here I gained insight into Full-Stack development, learned about database management and containerization.",
+            },
+          ],
+        },
+      },
+      references: {
+        h3: "References",
+        references: [
+          {
+            h4: "Uranus",
+            img: "/assets/uranus.jpg",
+            badges: [
+              "React Native",
+              "Expo",
+              "Nativewind"
+            ],
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum minima excepturi, velit odit dolorum sed expedita ducimus commodi reprehenderit, iure consectetur, voluptatem ex consequatur quia dolorem asperiores perspiciatis temporibus voluptas?"
+          },
+          {
+            h4: "Vagy idén vagy soha",
+            img: "/assets/vagyidenvagysoha.jpg",
+            badges: [
+              "React",
+              "Vite",
+              "MaterialUI",
+              "TailwindCSS"
+            ],
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum minima excepturi, velit odit dolorum sed expedita ducimus commodi reprehenderit, iure consectetur, voluptatem ex consequatur quia dolorem asperiores perspiciatis temporibus voluptas?"
+          },
+          {
+            h4: "Web Task Evaulator (WTE)",
+            img: "/assets/wte.jpg",
+            badges: [
+              "Vue",
+              "Pinia",
+              "FormKit",
+              "Laravel",
+              "TailwindCSS"
+            ],
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum minima excepturi, velit odit dolorum sed expedita ducimus commodi reprehenderit, iure consectetur, voluptatem ex consequatur quia dolorem asperiores perspiciatis temporibus voluptas?"
+          },
+          {
+            h4: "WTE - Chrome Extension",
+            img: "/assets/extension.jpg",
+            badges: [
+              "Vue",
+              "Pinia",
+              "FormKit",
+              "Laravel",
+              "TailwindCSS"
+            ],
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum minima excepturi, velit odit dolorum sed expedita ducimus commodi reprehenderit, iure consectetur, voluptatem ex consequatur quia dolorem asperiores perspiciatis temporibus voluptas?"
+          },
+        ],
+      },
+    },
+  },
 };
 
 export default translations;
--- src/views/References.jsx
+++ src/views/References.jsx
@@ -1,73 +1,23 @@
 import { Briefcase, BriefcaseBusiness, Folder, FolderKanban, Star, User } from "lucide-react";
 import useUI from "../hooks/useUI";
+import PrimaryContainer from "../components/PrimaryContainer";
+import PrimaryBadgeTitle from "../components/PrimaryBadgeTitle";
+import translations from "../utils/translations";
 
 function References() {
   const { theme, lang } = useUI();
+  const content = translations[lang]?.pages?.references;
 
-  const content = {
-    h3: "Referenciák",
-    references: [
-      {
-        h4: "Uranus",
-        img: "/assets/uranus.jpg",
-        badges: [
-          "React Native",
-          "Expo",
-          "Nativewind"
-        ],
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum minima excepturi, velit odit dolorum sed expedita ducimus commodi reprehenderit, iure consectetur, voluptatem ex consequatur quia dolorem asperiores perspiciatis temporibus voluptas?"
-      },
-      {
-        h4: "Vagy idén vagy soha",
-        img: "/assets/vagyidenvagysoha.jpg",
-        badges: [
-          "React",
-          "Vite",
-          "MaterialUI",
-          "TailwindCSS"
-        ],
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum minima excepturi, velit odit dolorum sed expedita ducimus commodi reprehenderit, iure consectetur, voluptatem ex consequatur quia dolorem asperiores perspiciatis temporibus voluptas?"
-      },
-      {
-        h4: "Web Task Evaulator (WTE)",
-        img: "/assets/wte.jpg",
-        badges: [
-          "Vue",
-          "Pinia",
-          "FormKit",
-          "Laravel",
-          "TailwindCSS"
-        ],
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum minima excepturi, velit odit dolorum sed expedita ducimus commodi reprehenderit, iure consectetur, voluptatem ex consequatur quia dolorem asperiores perspiciatis temporibus voluptas?"
-      },
-      {
-        h4: "WTE - Chrome Extension",
-        img: "/assets/wte.jpg",
-        badges: [
-          "Vue",
-          "Pinia",
-          "FormKit",
-          "Laravel",
-          "TailwindCSS"
-        ],
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum minima excepturi, velit odit dolorum sed expedita ducimus commodi reprehenderit, iure consectetur, voluptatem ex consequatur quia dolorem asperiores perspiciatis temporibus voluptas?"
-      },
-    ]
-  }
-
-  return (
-    <div className="w-full h-full px-10">
-      <div className={`${theme === "dark" ? "bg-gradient-to-br from-zinc-800 to-zinc-900 border border-zinc-800" : "bg-zinc-100"} p-10 block mx-auto space-y-10 max-w-[1000px] rounded-4xl shadow-xl`}>
-        <div className="flex flex-row items-center gap-2 bg-gradient-to-br from-blue-500 to-indigo-500 rounded-full py-3 px-8 w-fit mx-auto shadow-lg">
-          <FolderKanban size="25" />
-          <h3
-            className={`${theme === "dark" ? "text-zinc-100" : "text-zinc-700"} font-extrabold text-2xl uppercase`}
-          >
-            Referenciák
-          </h3>
-        </div>
+  return content && (
+    <section className="w-full h-full flex flex-col items-center">
+      <PrimaryContainer theme={theme}>
+        <PrimaryBadgeTitle
+          title={content?.h3}
+          icon={<FolderKanban />}
+          theme={theme}
+        />
         <div className="flex flex-col gap-y-10 sm:gap-y-20 min-h-screen">
-          {content.references.map((reference, index) => {
+          {content?.references.map((reference, index) => {
             return (
               <div key={reference.img} className={`flex flex-col-reverse ${index % 2 == 0 ? 'sm:flex-row' : "sm:flex-row-reverse"} gap-10`}>
                 <img src={reference.img} alt={reference.h4} className="border border-zinc-700 rounded-4xl w-full" />
@@ -76,7 +26,7 @@ function References() {
                   <div className="flex flex-wrap py-3 justify-center gap-2">
                     {reference.badges.map((item) => {
                       return (
-                        <span key={item} class="inline-flex items-center rounded-md bg-blue-400/10 px-2 py-1 text-xs font-medium text-blue-400 inset-ring inset-ring-blue-400/30">
+                        <span key={item} className="inline-flex items-center rounded-md bg-blue-400/10 px-2 py-1 text-xs font-medium text-blue-400 inset-ring inset-ring-blue-400/30">
                           {item}
                         </span>
                       )
@@ -88,8 +38,8 @@ function References() {
             );
           })}
         </div>
-      </div>
-    </div>
+      </PrimaryContainer>
+    </section>
   );
 }
 
