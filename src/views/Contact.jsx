7ef63c6fde63fe09d0b1fb4aa05b8382ca5d83f8
Chencsi <kevinkeeeh@gmail.com>
2025-09-17 10:53:38 +0200

    contact view added

---
+++ src/views/Contact.jsx
@@ -0,0 +1,23 @@
+import { Send } from "lucide-react";
+import PrimaryBadgeTitle from "../components/PrimaryBadgeTitle";
+import PrimaryContainer from "../components/PrimaryContainer";
+import useUI from "../hooks/useUI";
+import translations from "../utils/translations";
+
+const Contact = () => {
+  const { theme, lang } = useUI();
+  const content = translations[lang]?.pages?.contact;
+
+  return (
+    <section className="w-full h-full flex flex-col items-center">
+      <PrimaryContainer theme={theme}>
+        <PrimaryBadgeTitle
+          title={content?.h3}
+          icon={<Send />}
+        />
+      </PrimaryContainer>
+    </section>
+  );
+}
+
+export default Contact;
