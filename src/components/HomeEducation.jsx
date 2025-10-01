66d55a6e167a9aa3cc2bb02146d3cd3252f516e1
Chencsi <kevinkeeeh@gmail.com>
2025-10-01 17:56:15 +0200

    first nearly stable timeline

--- src/components/HomeEducation.jsx
+++ src/components/HomeEducation.jsx
@@ -1,22 +1,39 @@
+import { Book, Braces } from "lucide-react";
 import useUI from "../hooks/useUI";
 import translations from "../utils/translations";
-import { motion } from "framer-motion";
+import { motion, spring, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
-const EducationBox = ({ item, theme }) => {
+const EducationBox = ({ education, theme }) => {
   return (
     <div
-      key={item.h5}
-      className={`${theme === "dark" ? "bg-gradient-to-br from-zinc-800/80 to-zinc-900/80" : "bg-white/50"} max-w-sm backdrop-blur-xl py-5 px-5 rounded-2xl flex flex-col items-center gap-1 shadow-xl ${theme === "dark" && "shadow-zinc-950/30"}`}
+      key={education.h5}
+      className={`flex flex-row w-md sm:w-lg items-center`}
     >
-      <h5 class={`${theme === "dark" ? "text-zinc-50" : "text-zinc-700"} mb-2 text-2xl font-bold`}>
-        {item.h5}
-      </h5>
-      <p className={`${theme === "dark" ? "text-zinc-300" : "text-zinc-700"} mb-1 text-lg`}>
-        {item.p1}
-      </p>
-      <p className={`${theme === "dark" ? "text-zinc-300" : "text-zinc-600"}`}>
-        {item.p2}
-      </p>
+      <div className={`${theme === "dark" ? "bg-gradient-to-br from-zinc-800/80 to-zinc-900/80" : "bg-white/90"} rounded-l-2xl z-10 left-2 h-3/4 flex flex-col justify-center shadow-xl`}>
+        <p className={`${theme === "dark" ? "text-zinc-300" : "text-zinc-700"} text-xl [writing-mode:vertical-lr] rotate-180 font-thin p-1`}>
+          {education.p1}
+        </p>
+      </div>
+      <div
+        className={`${theme === "dark" ? "bg-gradient-to-br from-zinc-800/80 to-zinc-900/80" : "bg-white/90"} py-5 px-5 rounded-2xl backdrop-blur-xl w-full shadow-xl z-10 min-h-[250px] min-w-[300px]`}
+      >
+        <h5 className={`${theme === "dark" ? "text-zinc-50" : "text-zinc-700"} mb-2 text-2xl font-bold min-h-15`}>
+          {education.h5}
+        </h5>
+        <p className={`${theme === "dark" ? "text-zinc-300" : "text-zinc-600"}`}>
+          {education.p2}
+        </p>
+        <div className="flex flex-row gap-1 my-1">
+          {education?.badges?.map((badge) => {
+            return (
+              <span key={badge} className="inline-flex items-center rounded-md bg-blue-400/10 px-2 py-1 text-xs font-medium text-blue-400 inset-ring inset-ring-blue-400/30">
+                {badge}
+              </span>
+            )
+          })}
+        </div>
+      </div>
     </div>
   );
 }
@@ -24,32 +41,56 @@ const EducationBox = ({ item, theme }) => {
 function HomeEducation() {
   const { theme, lang } = useUI();
   const content = translations[lang]?.pages?.home?.education;
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end end"],
+  });
+
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-50%"]);
+
 
   return (
-    <>
+    <section ref={ref} className="relative h-[300vh] p-5 md:p-10 rounded-4xl">
       <motion.div
-        initial={{ y: 50, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.6 }}
-        className="w-full h-10"
+        className="sticky top-26 sm:top-30 h-[75vh] sm:h-[45rem] max-w-[1400px] mx-auto py-10 rounded-3xl shadow-xl overflow-hidden bg-gradient-to-br from-blue-500 to-indigo-500 flex flex-col gap-5"
+        transition={{ duration: 1, delay: 1, type: "spring" }}
       >
-      </motion.div>
-      <motion.section
-        initial={{ y: 50, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.6 }}
-        className="flex flex-col gap-2 max-w-[1000px] mx-auto px-5 sm:px-0">
-        <h3 className={`${theme === "dark" ? "text-zinc-300" : "text-zinc-700"} font-extrabold text-5xl uppercase text-shadow mb-5 text-center`}>
-          {content?.h3}
-        </h3>
+        <motion.div style={{ x }} className="w-[760%] md:w-[500%] lg:w-[4200px] h-full flex gap-20 lg:gap-35">
+          <div className="self-center w-[13%] md:w-[20%] lg:w-[30%] xl:w-[30%] flex flex-col items-center gap-5">
+            <div className="bg-white/20 rounded-full mx-auto px-8 py-3 text-zinc-100 font-extrabold text-4xl sm:text-4xl uppercase text-shadow text-center flex flex-row gap-2 items-center">
+              <Book />
+              <h3>{content?.h3}</h3>
+            </div>
+            <p className="text-zinc-100 text-center max-w-md md:max-w-2xl lg:max-w-4xl px-5 mx-auto text-xl font-semibold">
+              {content?.p}
+            </p>
+          </div>
+          <div className="flex flex-row gap-40 px-5 h-full relative">
+            {content?.educations?.slice().reverse().map((education, index) => {
+              const even = index % 2 == 0;
 
-        <div className="flex flex-row gap-5">
-          {content?.schools?.map((item) => (
-            <EducationBox item={item} theme={theme} />
-          ))}
-        </div>
-      </motion.section>
-    </>
+              return (
+                <div key={index} className={`${index === 0 && "ml-10"} flex ${even ? "flex-col-reverse" : "flex-col"} ${even && "justify-start"}`}>
+                  <EducationBox education={education} theme={theme} />
+                  <div className={`${even ? "-top-20 flex-col" : "-bottom-20 flex-col-reverse"} relative left-1/2 flex`}>
+                    <div className={`${theme === "dark" ? "bg-gradient-to-br from-zinc-800/80 to-zinc-900" : "bg-white/90"} w-2 h-20 left-0 absolute self-center`} />
+                    <div className={`${theme === "dark" ? "bg-gradient-to-br from-zinc-800 to-zinc-900" : "bg-white/90"} w-6 h-6 absolute left-0 -translate-x-1/3 rounded-full z-10`}></div>
+                    <p className={`text-2xl font-extrabold -left-5 absolute ${even ? "-top-10" : "-bottom-10"}`}>{education.p1.split("-")[0]}</p>
+                  </div>
+                </div>
+              );
+            })}
+            <div
+              initial={{ width: 0 }}
+              className="absolute top-1/2 -translate-y-10 h-20 w-full flex flex-col justify-center"
+            >
+              <div className={`${theme === "dark" ? "bg-gradient-to-r from-zinc-800/90 to-zinc-900/90" : "bg-white/90"} h-2 w-full rounded-full`} />
+            </div>
+          </div>
+        </motion.div>
+      </motion.div>
+    </section>
   );
 }
 
--- src/utils/translations.js
+++ src/utils/translations.js
@@ -1,4 +1,4 @@
-const sinceYear = new Date().getFullYear() - 2020;
+const sinceYear = new Date().getFullYear() - 2019;
 
 const translations = {
   hu: {
@@ -43,21 +43,42 @@ const translations = {
         ],
         about: {
           h3: "Bemutatkozás",
-          p1: `A nevem Kevin és ${sinceYear} éve kezdtem el foglalkozni programozással, azonban a programozás iránti érdeklődésem egészen általános iskolás koromra nyúlik vissza. Azóta abszolút elszánt vagyok iránta. Ez idő alatt egy szoftverfejlesztő és -tesztelő technikusi képzést végeztem el, most pedig az egyetemi éveim kezdem meg mérnökinformatikus szakon. Eddig számos iskolai és saját projektben vettem részt, ezekre pontosabban a <a href="/references" class="text-blue-400 hover:underline">referenciáknál</a> térek ki.`,
+          p1: `${sinceYear} éve kezdtem el foglalkozni programozással, azonban a programozás iránti érdeklődésem egészen általános iskolás koromra nyúlik vissza. Azóta több programozási nyelvvel és iránnyal találkoztam, de végül a webfejlesztés nőtt a szívemhez. Egy szoftverfejlesztő és -tesztelő technikusi képzést végeztem el, most pedig az első évem töltöm egyetemen mérnökinformatikus képzésen. Eddig számos iskolai és saját projektben vettem részt, ezekre pontosabban a <a href="/references" class="text-blue-400 hover:underline font-semibold">referenciáknál</a> térek ki.`,
           p2: "Célom, hogy felhasználóbarát weboldalakat és webalkalmazásokat építsek, amik valós problémákra találnak megoldást. Folyamatosan naprakészen tartom a tudástáram, hogy a legújabb technológiákat használjam.",
         },
         education: {
-          h3: "Tanulmányok",
-          schools: [
+          h3: "Tanulmányaim",
+          p: "A eddigi tudásom megszerzésében nagy szerepet játszott az oktatás amit kaptam az iskolában, azonban sok időt fordítottam mellette az önfejlesztésre, hogy megismerjem a modern és korszerű megoldásokat.",
+          educations: [
             {
               h5: "Gábor Dénes Egyetem",
               p1: "2025-",
               p2: "Jelenleg az első félévemet töltöm itt, mérnökinformatikus szakon és izgatottan várom, milyen további skilleket sajátíthatok még itt el.",
+              badges: [
+                
+              ]
             },
             {
               h5: "BMSZC Neumann János Informatikai Technikum",
               p1: "2020-2025",
-              p2: "Itt betekintést nyerhettem a Full-Stack fejlesztésbe, adatbázis kezelést és konténerizációt tanultunk.",
+              p2: "Itt betekintést nyerhettem a Full-Stack fejlesztésbe az alapoktól fogva a keretrendszerekig, majd adatbázis kezelést és konténerizációt is tanultunk.",
+              badges: [
+                "Web",
+                "C#",
+                "MYSQL",
+                "Docker",
+                "Linux"
+              ]
+            },
+            {
+              h5: "Első weboldalam és játékszerver pluginom",
+              p1: "2019",
+              p2: "Ekkor még általános iskolába jártam, azonban már aktívan elkezdtem nyári szünetben foglalkozni a programozással.",
+              badges: [
+                "HTML",
+                "CSS",
+                "JAVA"
+              ]
             },
           ],
         },
@@ -130,12 +151,12 @@ const translations = {
           { name: "Contact", path: "/contact" }
         ],
         settings: {
-          darkmode: "Dark mode",
+          darkmode: "Dark Mode",
           language: "Language",
         },
       },
       footer: {
-        p: "Code & design: Kevin Chen 💙 · © 2025"
+        p: "Code and design: Kevin Chen 💙 · © 2025"
       }
     },
     pages: {
@@ -149,67 +170,85 @@ const translations = {
         },
         highlights: [
           {
-            h3: "Responsive websites",
+            h3: "Responsive Websites",
             p: "Optimized appearance for all devices.",
           },
           {
-            h3: "Modern technologies",
+            h3: "Modern Technologies",
             p: "React, Angular, Framer Motion and TailwindCSS",
           },
           {
-            h3: "UI/UX focus",
-            p: "Clean and user-friendly user experience.",
+            h3: "UI/UX Focus",
+            p: "Clean and user-friendly experience.",
           },
         ],
         about: {
-          h3: "About me",
-          p1: `My name is Kevin and I started programming ${sinceYear} years ago, but my interest in programming goes back to elementary school. Since then, I have been absolutely dedicated to it. During this time, I completed a software developer and tester technician course, and now I am starting my university years as an engineering IT student. So far, I have participated in several school and personal projects, which I detail more precisely in the <a href="/references" class="text-blue-400 hover:underline">references</a> section.`,
-          p2: "My goal is to build user-friendly websites and web applications that solve real problems. I constantly keep my knowledge up to date to use the latest technologies.",
+          h3: "About Me",
+          p1: `My name is Kevin and I've been programming for ${sinceYear} years, although my interest in programming dates back to elementary school. I've been absolutely dedicated to it ever since. During this time, I completed a software developer and tester technical training, and now I'm starting my university years in computer engineering. So far, I've participated in numerous school and personal projects, which I detail more specifically in the <a href="/references" class="text-blue-400 hover:underline">references</a> section.`,
+          p2: "My goal is to build user-friendly websites and web applications that solve real problems. I constantly keep my knowledge up-to-date to use the latest technologies.",
         },
         education: {
-          h3: "Education",
-          schools: [
+          h3: "My Education",
+          educations: [
             {
-              h5: "Gábor Dénes University",
+              h5: "Dennis Gabor College",
               p1: "2025-",
-              p2: "Currently in my first semester here as an engineering IT student, and I am excited to see what further skills I can acquire.",
+              p2: "I'm currently spending my first semester here in computer engineering and I'm excited to see what additional skills I can acquire here.",
+              badges: [
+                
+              ]
             },
             {
-              h5: "BMSZC Neumann János Informatics Technical School",
+              h5: "BMSZC Neumann Technical School of Informatics",
               p1: "2020-2025",
-              p2: "Here I gained insight into Full-Stack development, learned about database management and containerization.",
+              p2: "Here I gained insight into Full-Stack development from the basics to frameworks, then we also learned database management and containerization.",
+              badges: [
+                "Web",
+                "C#",
+                "MYSQL",
+                "Docker",
+                "Linux"
+              ]
+            },
+            {
+              h5: "My first website and game server plugin",
+              p1: "2019",
+              p2: "I was still in elementary school at this time, but I had already actively started to focus on programming during summer break.",
             },
           ],
         },
       },
       references: {
         h3: "References",
-        p: "I would like to highlight my projects below, most of which were created in a team. You can see these and the details through the GitHub repositories.",
+        p: "I would like to highlight my projects below, mostly made in teams. These and the details can be seen through the GitHub repositories.",
         references: [
           {
             h4: "Uranus",
-            img: "/assets/uranus.jpg",
+            img: "/uranus-splash.png",
+            size: "sm",
             badges: [
               "React Native",
               "Expo",
               "Nativewind"
             ],
-            description: "This is a mobile application that allows you to use Neptun in app form with a single login."
+            description: "This is a mobile application that allows you to use Neptun in application form with a single sign-on."
           },
           {
             h4: "Vagy idén vagy soha",
-            img: "/assets/vagyidenvagysoha.jpg",
+            img: "/vagyidenvagysoha.png",
+            size: "md",
             badges: [
               "React",
               "Vite",
               "MaterialUI",
               "TailwindCSS"
             ],
-            description: "A landing page I am creating on commission for a private education entrepreneur. The appearance is a modern and clean single page, which is completely static."
+            description: "A landing page that I'm creating by request for a private education entrepreneur. The appearance is a modern and clean single page that is completely static."
           },
           {
-            h4: "Web Task Evaulator (WTE)",
-            img: "/assets/wte.jpg",
+            h4: "Web Task Evaluator (WTE)",
+            img: "/wte-desktop.png",
+            size: "md",
             badges: [
               "Vue",
               "Pinia",
@@ -217,23 +256,27 @@ const translations = {
               "Laravel",
               "TailwindCSS"
             ],
-            description: "With this web application, we wanted to make the daily tasks of our web teachers at my former school easier by automating the checking and correction of HTML and CSS assignments. I was not responsible for the underlying logic, I participated in the UI, some API endpoints, and automated testing."
+            description: "With this web application, we wanted to make the daily tasks of our web teachers in our former school easier by automating the checking and correction of HTML and CSS task sets. I wasn't responsible for the underlying logic; I participated in the appearance, some API endpoints, and automated testing."
           },
           {
             h4: "WTE - Chrome Extension",
-            img: "/assets/extension.jpg",
+            img: "/extension.jpeg",
+            size: "[50px]",
             badges: [
               "Angular",
               "RxJS",
               "Lucide",
               "TailwindCSS"
             ],
-            description: "This project is the student-side part of the WTE web application, which is a browser pop-up so that students can continuously track their progress in solving the task. In this project, the logic of the main project is also implemented for checking, but I was responsible for the user interface."
-          }
+            description: "This project is the student side of the WTE web application, which is a pop-up in the browser so that students can continuously track where they are in solving the task. In this project, the logic of the big brother project is similarly implemented for checking, however, I was responsible for the user interface display here."
+          },
         ],
       },
+      contact: {
+        h3: "Contact",
+      }
     },
-  },
+  }
 };
 
 export default translations;
