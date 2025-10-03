ae9d41a9cf61df9a6b982f11cd5ce939b5d41ad2
Chencsi <kevinkeeeh@gmail.com>
2025-10-03 13:21:02 +0200

    using the new button

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,15 +1,20 @@
 import PrimaryButton from "../components/PrimaryButton";
+import useUI from "../hooks/useUI";
+import translations from "../utils/translations";
 
 function NotFound() {
-	return (
-			<div className="h-[70vh] flex flex-col items-center justify-center gap-3">
-				<h1 className="text-4xl font-extrabold">Hoppá!</h1>
-				<p>Sajnos az általad keresett oldal nem létezik. :(</p>
-				<PrimaryButton link="/">
-          Vissza a főoldalra
-        </PrimaryButton>
-			</div>
-	);
+  const { lang } = useUI();
+  const content = translations[lang]?.pages?.notfound;
+
+  return (
+    <div className="h-[70vh] flex flex-col items-center justify-center gap-3">
+      <h1 className="text-4xl font-extrabold">{content?.h1}</h1>
+      <p>{content?.p}</p>
+      <PrimaryButton link="/">
+        {content?.button}
+      </PrimaryButton>
+    </div>
+  );
 }
 
 export default NotFound;
