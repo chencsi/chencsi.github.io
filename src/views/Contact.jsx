927fb2d83616e392f0807c03dac8a28cb07e65bc
Chencsi <kevinkeeeh@gmail.com>
2025-10-17 22:59:09 +0200

    pdf download and quick view

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -11,6 +11,7 @@ const Contact = () => {
   const { theme, lang } = useUI();
   const content = translations[lang]?.pages?.contact;
   const [isViewerOpen, setIsViewerOpen] = useState(false);
+  const cvPath = "pulbic/kevin_chen_en.pdf"
 
   const handleCloseViewer = () => {
     setIsViewerOpen(false);
@@ -30,7 +31,7 @@ const Contact = () => {
             />
             <p className={`${theme === "dark" ? "text-zinc-300" : "text-zinc-700"} text-center`}>{content?.p}</p>
             <ContactForm content={content?.form} theme={theme} />
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-5 p-5">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5 py-5 sm:px-5">
               <div className="space-y-5">
                 <h4 className="font-black text-3xl">{content?.section1?.h4}</h4>
                 <div className="flex flex-row gap-3">
@@ -49,23 +50,27 @@ const Contact = () => {
 
               <div className="space-y-5">
                 <h4 className="font-black text-3xl">{content?.section2?.h4}</h4>
-                <div className="flex flex-row gap-5 select-none">
-                  <motion.div
+                <div className="flex flex-row gap-5 sm:gap-5 select-none">
+                  <a href={cvPath} download="kevin_chen_en.pdf">
+                    <motion.button
+                      type="button"
+                      whileHover={{
+                        scale: 1.075,
+                        transition: {
+                          type: "spring",
+                          stiffness: 300,
+                        },
+                      }}
+                      whileTap={{ scale: 0.995 }}
+                      className={`border-2 p-2 rounded-2xl cursor-pointer flex flex-col gap-2 items-center w-[7rem] sm:w-[8rem] transition-colors duration-300 ${theme === "dark" ? "hover:text-zinc-800 hover:bg-zinc-50" : "hover:text-zinc-50 hover:bg-zinc-800"}`}>
+                      <File />
+                      <p>{content?.section2?.item1}</p>
+                    </motion.button>
+                  </a>
+                  <motion.button
+                    type="button"
                     whileHover={{
-                      scale: 1.1,
-                      transition: {
-                        type: "spring",
-                        stiffness: 300,
-                      },
-                    }}
-                    whileTap={{ scale: 0.995 }}
-                    className={`border-2 p-2 rounded-2xl cursor-pointer flex flex-col gap-2 items-center w-[8rem] transition-colors duration-300 ${theme === "dark" ? "hover:text-zinc-800 hover:bg-zinc-50" : "hover:text-zinc-50 hover:bg-zinc-800"}`}>
-                    <File />
-                    <p>{content?.section2?.item1}</p>
-                  </motion.div>
-                  <motion.div
-                    whileHover={{
-                      scale: 1.05,
+                      scale: 1.075,
                       transition: {
                         type: "spring",
                         stiffness: 300,
@@ -75,10 +80,10 @@ const Contact = () => {
                     onClick={() => {
                       handleOpenViewer();
                     }}
-                    className={`border-2 p-2 rounded-2xl cursor-pointer flex flex-col gap-2 items-center w-[8rem] transition-colors duration-300 ${theme === "dark" ? "hover:text-zinc-800 hover:bg-zinc-50" : "hover:text-zinc-50 hover:bg-zinc-800"}`}>
+                    className={`border-2 p-2 rounded-2xl cursor-pointer flex flex-col gap-2 items-center w-[7rem] sm:w-[8rem] transition-colors duration-300 ${theme === "dark" ? "hover:text-zinc-800 hover:bg-zinc-50" : "hover:text-zinc-50 hover:bg-zinc-800"}`}>
                     <View />
                     <p>{content?.section2?.item2}</p>
-                  </motion.div>
+                  </motion.button>
                 </div>
               </div>
             </div>
@@ -92,7 +97,7 @@ const Contact = () => {
             <div className="mx-5 sm:mx-10">
               <motion.div
                 className={
-                  `${theme === "dark" ? "text-zinc-300 bg-gradient-to-br from-zinc-800 to-zinc-900" : "bg-white text-zinc-900"} p-5 flex flex-col gap-5 min-h-[50rem] max-w-[1000px] mx-auto rounded-4xl shadow-xl h-full`}
+                  `${theme === "dark" ? "text-zinc-300 bg-gradient-to-br from-zinc-800 to-zinc-900" : "bg-white text-zinc-900"} p-5 flex flex-col gap-5 min-h-[90vh] max-h-[90vh] max-w-[1000px] mx-auto rounded-4xl shadow-xl h-full`}
                 initial={{ opacity: 0, y: 50 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true }}
@@ -101,13 +106,16 @@ const Contact = () => {
                   ease: "easeOut"
                 }}
               >
-                <motion.button onClick={handleCloseViewer} type="button" className="p-2 w-fit self-end rounded-full cursor-pointer focus:outline-none">
-                  <X />
-                </motion.button>
+                <div className="flex flex-row items-center">
+                  <h5 className="flex-1 p-2">Megtekintő</h5>
+                  <motion.button onClick={handleCloseViewer} type="button" className="w-fit text-end self-end rounded-full cursor-pointer focus:outline-none p-2 hover:bg-zinc-700 transition">
+                    <X />
+                  </motion.button>
+                </div>
                 <iframe
-                  src="kevin_chen_en.pdf"
-                  className="rounded-2xl w-full h-full flex-1"
-                  style={{ minHeight: "100%" }}
+                  src="kevin_chen_en.pdf?page=hsn#toolbar=0"
+                  className="w-full h-full flex-1"
+                  toolbar="0"
                 />
               </motion.div>
             </div>
