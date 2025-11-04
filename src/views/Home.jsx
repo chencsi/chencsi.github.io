accb7c70e82789e6bcf0395f206ae26bf4936817
Chencsi <kevinkeeeh@gmail.com>
2025-11-04 12:31:27 +0100

    refactor home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,16 +1,16 @@
-import HomeAbout from "../components/Home/About";
-import HomeEducation from "../components/Home/Education";
-import HomeHero from "../components/Home/Hero";
-import HomeHighlights from "../components/Home/Highlights";
-import HomeSkills from "../components/Home/Skills";
+import About from "../components/Home/About";
+import Education from "../components/Home/Education";
+import Hero from "../components/Home/Hero";
+import Highlights from "../components/Home/Highlights";
+import Skills from "../components/Home/Skills";
 
 function Home() {
   return (
     <div className="h-full flex flex-col gap-y-24 md:gap-y-34">
-      <HomeHero />
-      <HomeHighlights />
-      <HomeAbout />
-      <HomeSkills />
+      <Hero />
+      <Highlights />
+      <About />
+      <Skills />
     </div>
   );
 }
